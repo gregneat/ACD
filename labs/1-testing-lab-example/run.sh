65e563573c862e6ef00cd757a1ad6794cef6e83c
dylanwilbur <dylan.m.w@icloud.com>
2018-07-13 11:23:17 -0700

    Add (hopefully) windows compatible bash files

--- labs/1-testing-lab-example/run.sh
+++ labs/1-testing-lab-example/run.sh
@@ -2,16 +2,7 @@
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 cd "$DIR"
 
-# buildClasspath() {
-#   appDir=$1
-#   classpath=.
-#   for jar in $appDir/lib/*
-#   do
-#       classpath=$classpath:$jar
-#   done
-# }
-# buildClasspath "$DIR"
-java MoveRect
+java Calculator
 
 echo 'Press enter to close'
 read
